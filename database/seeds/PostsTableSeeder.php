a1d1c1e9f6ee76205eb343e734baaa83de825470
Welliton Klein <welliton.fokushima@gmail.com>
2018-02-14 18:30:56 -0200

    Criando Model de Relacionamento

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use \App\Post;
 
-class DatabaseSeeder extends Seeder
+class PostsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(PostsTableSeeder::class);
+        Post::truncate();
+
+        factory(Post::class,10)->create();
     }
 }
