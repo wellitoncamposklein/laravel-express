5b6116af7d50dc2faef65e318f2b65e021239c8a
Welliton Klein <welliton.fokushima@gmail.com>
2018-02-14 20:18:13 -0200

    Praticando relacionamentos ManyToMany

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use \Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        //disable foreign key check for this connection before running seeders
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
          $this->call(PostsTableSeeder::class);
+         $this->call(TagTableSeeder::class);
+
+        // supposed to only apply to a single connection and reset it's self
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
