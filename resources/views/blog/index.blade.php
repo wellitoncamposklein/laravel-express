28638a3ebd6cecb8256f3ebd2501f0ddc7b77c8f
Welliton Klein <welliton.fokushima@gmail.com>
2018-02-14 20:25:02 -0200

    Listando utilizando relacionamentos

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -8,6 +8,13 @@
     @foreach($posts as $post)
         <h3>{{ $post->title }}</h3>
         <p>{{ $post->content }}</p>
+        <b>Tags:</b>
+        <ul>
+            @foreach($post->tags as $tag)
+                <li>{{ $tag->name }}</li>
+            @endforeach
+        </ul>
+
         <h5>Comments</h5>
         @foreach($post->comments as $comment)
             <b>{{ $comment->name }}: </b>{{ $comment->comment }}
